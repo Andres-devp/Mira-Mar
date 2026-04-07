f2de9b3d93a6b5030d1a6e03141e58ff6b07145d
Nicolas <nicolasangulof@gmail.com>
2026-04-07 12:55:00 -0500

    arreglar error de package

--- SprintBoot Web/demo/src/main/java/com/example/demo/controller/ReservationController.java
+++ SprintBoot Web/demo/src/main/java/com/example/demo/controller/ReservationController.java
@@ -1,4 +1,4 @@
-package main.java.com.example.demo.controller;
+package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
